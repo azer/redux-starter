0a8deb8ced0495b399420a13b0da60d1beab8802
E.Azer Koçulu <azer@kodfabrik.com>
2015-11-01 15:27:42 -0800

    loose base starter version

--- example.js
+++ example.js
@@ -4,7 +4,8 @@ var redux = require("./");
 var myproject = kik({
   name: 'yolo',
   folder: '/tmp/yolo',
-  starters: [require('./')],
+  //starters: [{ module: require('./'), subfolder: 'ui' }],
+  starters:[require('./')],
   remote: 'azer/yolo',
   context: {
     github: 'azer'
